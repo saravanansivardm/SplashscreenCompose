99aef8fee70e02b9958a1ab5c56b8413e79874b9
Siva Saravanan <sivasaravananrdm@gmail.com>
2024-02-01 14:58:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreencompose/SplashActivityAPI.kt
@@ -0,0 +1,28 @@
+package com.example.splashscreencompose
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivityAPI : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(50)
+            val intent = Intent(this@SplashActivityAPI, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
